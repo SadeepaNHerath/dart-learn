7cdebb3abe03cd4d06db7b73bf85ade0c3c89f9e
SadeepaNHerath <sadeepahearth@gmail.com>
2024-11-30 22:34:40 +0530

    new

---
+++ string-concat.dart
@@ -0,0 +1,23 @@
+void main() {
+  //Dart is a strong typed language
+
+  /* This is 
+  Multiline comment
+  */
+
+  print("3" + "2"); // prints 32
+
+  print(3 + 2); //prints 5
+
+  //print("3"+2); error: cannot concatenate a string and an integer
+
+  print("3" + 2.toString()); // prints 32
+
+  print("3" * 3); // prints 333
+
+  //print("3"-"2"); error: invalid operator '-' for the types 'String'
+
+  print(3 - 2); //prints 1
+
+  //Only + and * can be used for string concatenation and multiplication
+}
